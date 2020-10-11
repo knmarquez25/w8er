2e1c83d1309ede4e8d45690893e10bee9a69f855
at235am <at235am@gmail.com>
2020-10-11 12:32:06 -0700

    Updates

---
+++ precache-manifest.a49f822bfc93fc156d89f5c413cd8ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a8c9572f1d0754cc3234b22f2469f0",
+    "url": "/w8er/index.html"
+  },
+  {
+    "revision": "acd6dfd6d2b2fd0e6f55",
+    "url": "/w8er/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5bf14c30bc645a4ab59f",
+    "url": "/w8er/static/js/2.08971a5a.chunk.js"
+  },
+  {
+    "revision": "db30d36cc67fe41fff60f7a6d4e2e2fd",
+    "url": "/w8er/static/js/2.08971a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd6dfd6d2b2fd0e6f55",
+    "url": "/w8er/static/js/main.e5c1aec6.chunk.js"
+  },
+  {
+    "revision": "fbe6829d2984f3c66345",
+    "url": "/w8er/static/js/runtime-main.8a2f96ab.js"
+  }
+]);
