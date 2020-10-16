bd2c5575b9548c2d2715241cb8ab4e4120da452c
Marcos Rodriguez <marcos.rodriguez@student.csulb.edu>
2020-10-15 22:01:18 -0700

    Reservation outline

--- frontend/src/pages/ReservationPage.jsx
+++ frontend/src/pages/ReservationPage.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 import { css, jsx } from "@emotion/core";
 import styled from "@emotion/styled";
+import FormInput from "../components/inputs/FormInput";
 
+const RContainer = styled.div`
+    background-color: ${({ theme }) => theme.colors.background};
+    width: 100%;
+    height: 100%;
 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FormContainer = styled.form`
+    background-color: ${({ theme }) => theme.colors.surface};
+
+    width: 40%;
+    height: 80%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* overflow: hidden; */
+    overflow: auto;
+`;
+
+const RFormInput = styled(FormInput)`
+    min-width: 80%;
+    
+    margin-bottom: 2rem;
+`;
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
@@ -11,25 +41,17 @@ const Title = styled.h1`
 
 `;
 
-const Wrapper = styled.section`
-    background-color: ${({ theme }) => theme.colors.background};
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: column;
-    background: papayawhip;
-`;
-
 
 const ReservationPage = () => {
 
 return(
-    <Wrapper>
-        <Title>
-            Hello World!
-        </Title>
-    </Wrapper>
+    <RContainer>
+        <FormContainer>
+            <RFormInput>
+                
+            </RFormInput>
+        </FormContainer>
+    </RContainer>
     );
 };
 export default ReservationPage;
