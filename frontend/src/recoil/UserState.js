2dfaeda219b2ffef08dee9d9d028859e0f6c574a
kathya marquez <knmarquez25@gmail.com>
2020-10-17 18:33:41 -0700

    added gues_floormap.jsx

---
+++ frontend/src/recoil/UserState.js
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+export const userState = atom({
+  key: "userState",
+  default: { cool: "test" }, // use this when testing restaurant professional stuff
+  // default: null,
+});
