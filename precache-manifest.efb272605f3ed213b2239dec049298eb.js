81777371bcfc5250a07ee27641b2a801e05f3db6
kathya marquez <knmarquez25@gmail.com>
2020-10-19 21:42:12 -0700

    Updates

---
+++ precache-manifest.efb272605f3ed213b2239dec049298eb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57aabc045bdcd4fbe17e21c6514cf823",
+    "url": "/w8er/index.html"
+  },
+  {
+    "revision": "6b06cbe737124fa545bf",
+    "url": "/w8er/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3270e28ddf88210f1735",
+    "url": "/w8er/static/js/2.542f94f4.chunk.js"
+  },
+  {
+    "revision": "23955be43b796a130abcd85ae385ec27",
+    "url": "/w8er/static/js/2.542f94f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b06cbe737124fa545bf",
+    "url": "/w8er/static/js/main.56e86500.chunk.js"
+  },
+  {
+    "revision": "fbe6829d2984f3c66345",
+    "url": "/w8er/static/js/runtime-main.8a2f96ab.js"
+  },
+  {
+    "revision": "0366d8ff13e3e1a27f2b821c813a56c7",
+    "url": "/w8er/static/media/add-to-waitlist.0366d8ff.gif"
+  },
+  {
+    "revision": "017ad008d128f0c6ee527bac96934507",
+    "url": "/w8er/static/media/assign-seats.017ad008.gif"
+  },
+  {
+    "revision": "37e997b722df8ed38e1ccb50431dc2c0",
+    "url": "/w8er/static/media/circle.37e997b7.svg"
+  },
+  {
+    "revision": "5e09ece6910218f68b747dbdb39ec4ac",
+    "url": "/w8er/static/media/dashboard.5e09ece6.svg"
+  },
+  {
+    "revision": "bcd86cef63ba64c14b5f358a8eae47f1",
+    "url": "/w8er/static/media/edit-floor-map.bcd86cef.gif"
+  },
+  {
+    "revision": "c25571005a13fe8111cbc8cccf9706fd",
+    "url": "/w8er/static/media/half-circle.c2557100.svg"
+  },
+  {
+    "revision": "a53259e856797512c51f1d5dfa14db27",
+    "url": "/w8er/static/media/link-preview.a53259e8.png"
+  },
+  {
+    "revision": "f2a3b9626fb1fa2ce5663d758463b8b7",
+    "url": "/w8er/static/media/lshape.f2a3b962.svg"
+  },
+  {
+    "revision": "4f409918d133f841b3580881c4ad4948",
+    "url": "/w8er/static/media/move-sub-lists.4f409918.gif"
+  },
+  {
+    "revision": "5441cb6e4d028f97d2b22ee1a34c64ab",
+    "url": "/w8er/static/media/queue.5441cb6e.svg"
+  },
+  {
+    "revision": "1523d73f3514650288ae19adb0980315",
+    "url": "/w8er/static/media/rectangle.1523d73f.svg"
+  },
+  {
+    "revision": "04b1034d9777405b2b38fff2944764be",
+    "url": "/w8er/static/media/square.04b1034d.svg"
+  },
+  {
+    "revision": "975f0c437f72fbfa07bbc51aeb2c93b4",
+    "url": "/w8er/static/media/triangle.975f0c43.svg"
+  },
+  {
+    "revision": "c1667a4abde2540d606b92b1e91ca151",
+    "url": "/w8er/static/media/w8r-logo.c1667a4a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/w8er/precache-manifest.7474083bad7d8c0a6f7ff2b2ff5f19e6.js"
+  "/w8er/precache-manifest.efb272605f3ed213b2239dec049298eb.js"
 );
 
 self.addEventListener('message', (event) => {
